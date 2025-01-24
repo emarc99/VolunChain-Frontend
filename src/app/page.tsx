f7ebf0b03df40368387f9d964a79f34b31764599
KevinLatino <latinokevin9@gmail.com>
2025-01-24 01:05:33 -0600

    fix: merge conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import Footer from "@/components/Footer";
-
+import OurMission from "@/components/OurMissionComponents/OurMission";
 
 export default function Home() {
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 lg:p-30 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Footer />
+    <div className="mt-[40rem] grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-4 lg:p-30 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <OurMission />
     </div>
   );
 }
