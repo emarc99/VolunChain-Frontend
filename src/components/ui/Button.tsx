8215a8c4042ffcd4bfcb149a301f25b5497226db
KevinLatino <latinokevin9@gmail.com>
2025-01-24 09:48:04 -0600

    feat: adding className props to some components

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,33 +4,36 @@ interface ButtonProps {
   children: React.ReactNode | string;
   onClick?: () => void;
   variant: "primary" | "secondary";
-  type: "button" | "submit" | "reset";
+  type?: "button" | "submit" | "reset";
   textColor?: "white" | "black" | "primary" | "secondary";
+  className?: string;
 }
 
 const Button = ({
   children,
   onClick,
   variant,
-  textColor,
+  textColor = "white",
   type = "button",
+  className,
 }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={clsx("cursor-pointer py-3 px-6 rounded-full w-auto font-semibold border-2", {
-        
-        "text-white": textColor === "white",
-        "text-black": textColor === "black",
-        "text-primary": textColor === "primary",
-        "text-secondary": textColor === "secondary",
-
-        "bg-primary ":
-          variant === "primary",
-        "border-primary": variant === "primary",
-        "border-2 border-secondary":
-          variant === "secondary",
-      })}
+      className={clsx(
+        "cursor-pointer py-3 px-6 rounded-full w-auto font-semibold border-2",
+        {
+          "text-white": textColor === "white",
+          "text-black": textColor === "black",
+          "text-primary": textColor === "primary",
+          "text-secondary": textColor === "secondary",
+        },
+        {
+          "bg-primary border-primary": variant === "primary",
+          "bg-secondary border-secondary": variant === "secondary",
+        },
+        className
+      )}
       onClick={onClick}
     >
       {children}
