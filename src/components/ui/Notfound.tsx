4f7a2ca2a0449c81a85c321535058458ca5e225c
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2025-01-26 19:31:12 -0600

    Update Notfound.tsx

--- src/components/ui/Notfound.tsx
+++ src/components/ui/Notfound.tsx
@@ -6,14 +6,16 @@ import Image from "next/image";
 import {useRouter} from "next/navigation";
 import {LucideArrowLeft} from "lucide-react";
 const Notfound = () => {
+
    const router = useRouter();
    const handleBack = () => {
       router.back();
    };
+
    return (
-      <div className="  min-h-[90vh] sm:min-h-screen h-auto flex justify-center items-center pt-20  relative ">
+      <div className="flex flex-col justify-center items-center w-full h-[100vh]">
          <Image src={NotFound} alt="not-found" />
-         <section className="  absolute bottom-8 sm:bottom-[-1.5rem] flex justiify-center items-center flex-col w-full">
+         <section className="bottom-8 sm:bottom-[-1.5rem] flex justiify-center items-center flex-col w-full">
             <p className="text-secondary text-[18px] font-[800] text-center"> 404 - Page Not Found</p>
             <br />
             <Button variant="primary" textColor="secondary" className=" m-auto flex gap-2" onClick={handleBack}>
