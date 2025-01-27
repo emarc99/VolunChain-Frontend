87dbfc07dfb58a261ead662bd4b8ec629e11cfc6
Robert <rsft6000@gmail.com>
2025-01-27 13:45:36 -0600

    feat/foundation-register: page created and new fields added to form

---
+++ src/icons/DownArrow.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const DownArrow: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg width="20" height="17" viewBox="0 0 28 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M26 2L14 14L2 2" stroke="#73B9EB" strokeWidth="3" strokeLinecap="round" />
+  </svg>
+
+);
+
+export default DownArrow;
